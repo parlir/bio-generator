5b120c1edd8160375acdf1853e3bdfa90b617040
Ralph Parkison <chesslunatic@gmail.com>
2020-07-18 20:49:23 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const ACTION = [
+  "driving",
+  "executing",
+  "passionate about",
+  "humanizing",
+  "provoking new",
+  "creating",
+];
+const SUBJECT = ["executive", "leader", "entrepreneur", "innovator"];
+const ADJECTIVE = [
+  "compassionate",
+  "empathetic",
+  "thoughtful",
+  "meaningful",
+  "value-based",
+  "global",
+  "caring",
+  "technical",
+  "revolutionary",
+  "impactful",
+  "industry leading",
+];
+const SUBJECT_INDUSTRY = ["products", "ideas", "technologies"];
+
+function getRandomEntry(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+function generate(workArea) {
+  const adjective = getRandomEntry(ADJECTIVE);
+  const subject = getRandomEntry(SUBJECT);
+  const action = getRandomEntry(ACTION);
+
+  const product_subject = getRandomEntry(SUBJECT_INDUSTRY);
+  const product_adjective = getRandomEntry(ADJECTIVE);
+
+  return `${adjective} ${subject} ${action} ${product_adjective} ${workArea} ${product_subject}`;
+}
 
 function App() {
+  const [industry, setIndustry] = useState("social media");
+  const [bio, setBio] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div>
+          <h3>Work Industry</h3>
+          <input
+            value={industry}
+            onChange={(e) => {
+              const value = e.target.value;
+              console.log(e, e.target.value);
+              setIndustry(value);
+            }}
+          ></input>
+        </div>
+      </div>
+      <div>
+        <h3>Generated BIO:</h3>
+        <textarea
+          readOnly
+          value={bio}
+          style={{ width: "400px", height: "100px" }}
+        />
+      </div>
+
+      <button
+        onClick={() => {
+          setBio(generate(industry));
+        }}
+      >
+        <h2>Generate</h2>
+      </button>
     </div>
   );
 }
